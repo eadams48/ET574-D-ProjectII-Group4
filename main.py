98a748191cb3f346c57f6791dbd88979ab0bea9c
imadfuad <imaduddin778@gmail.com>
2026-04-21 02:07:15 -0400

    Add factorial and area_of_circle functions

--- main.py
+++ main.py
@@ -19,3 +19,37 @@ def Annual_Compd_Intrst(P, r, n, t):
         return "Invalid input"
 
     return P * (1 + r / n) ** (n * t)
+def factorial(x):
+    """
+    Name: factorial
+    Purpose: Calculates the factorial of a non-negative integer
+    Parameters:
+        x(int): a non-negative integer
+    Return:
+        int: factorial of x
+    """
+
+    if not isinstance(x, int) or x < 0:
+        return "Invalid input"
+
+    result = 1
+    for i in range(1, x + 1):
+        result = result * i
+    return result
+
+
+def area_of_circle(r):
+    """
+    Name: area_of_circle
+    Purpose: Calculates the area of a circle
+    Parameters:
+        r(int or float): radius of the circle
+    Return:
+        float: area of the circle
+    """
+
+    if r < 0:
+        return "Invalid input"
+
+    pi = 3.14159
+    return pi * r * r
